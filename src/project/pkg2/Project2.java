1e4ceea5e236940fd31695dc2ef1d5e5dce61358
Muhammad Farhan <Muhammad Farhan@LAPTOP-6I5DS4U1>
2018-01-17 23:00:55 +0800

    version 1

---
+++ src/project/pkg2/Project2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg2;
+
+/**
+ *
+ * @author Muhammad Farhan
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Noor Aiman
+ */
+public class project2 {
+    public static void main (String args[])
+    {
+        System.out.println("Kepercayaan kepada Tuhan");
+        System.out.println("Kesetiaan kepada Raja dan Negara");
+        System.out.println("Keluhuran Perlembagaan");
+        System.out.println("Kedaulatan Undang-Undang");
+        System.out.println("Kesopanan dan Kesusilaan");
+    }
+    
+}
